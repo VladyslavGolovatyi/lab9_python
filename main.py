fcf7b652b2c01633a8cd0f8e94d56f59cdc1bdb8
Name Surname <info@developerS2.com>
2022-05-24 13:51:24 +0300

    Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from vehicles import *
+
+car = Car(70, 5, "sedan", "Skoda Fabia")
+car.print_info()
+
+print()
+
+bus = Bus(50, 25, False)
+bus.print_info()
+
+print()
+
+plane = Plane(150, 100, 2000)
+plane.print_info()
+
+print()
+
+trolleybus = Trolleybus(75, 35, 6)
+trolleybus.print_info()
+
+print()
+
+tram = Tram(45, 20, 2010)
+tram.print_info()
